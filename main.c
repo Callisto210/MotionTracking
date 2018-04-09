eff4301aa3a63cf85ab8cfa70dc0e67dfcd620e8
Callisto210 <mikro16@o2.pl>
2018-04-09 22:31:05 +0200

    Add compile script and update api to appropriate version

--- main.c
+++ main.c
@@ -9,70 +9,88 @@
 using namespace std;
 using namespace cv;
 
+Ptr<Tracker> createTrackerByName(string trackerType) {
+
+        if (trackerType == "BOOSTING")
+            return TrackerBoosting::create();
+        if (trackerType == "MIL")
+            return TrackerMIL::create();
+        if (trackerType == "KCF")
+            return TrackerKCF::create();
+        if (trackerType == "TLD")
+            return TrackerTLD::create();
+        if (trackerType == "MEDIANFLOW")
+            return TrackerMedianFlow::create();
+        if (trackerType == "GOTURN")
+            return TrackerGOTURN::create();
+}
+
 int main( int argc, char** argv ){
-  // show help
-  if(argc<2){
-    cout<<
-      " Usage: example_tracking_multitracker <video_name> [algorithm]\n"
-      " examples:\n"
-      " example_tracking_multitracker Bolt/img/%04d.jpg\n"
-      " example_tracking_multitracker faceocc2.webm MEDIANFLOW\n"
-      << endl;
-    return 0;
-  }
+	if(argc<2){
+	cout<<
+	" Usage: example_tracking_multitracker <video_name> [algorithm]\n"
+	" examples:\n"
+	" example_tracking_multitracker Bolt/img/%04d.jpg\n"
+	" example_tracking_multitracker faceocc2.webm MEDIANFLOW\n"
+	<< endl;
+	return 0;
+	}
 
-  // set the default tracking algorithm
-  std::string trackingAlg = "KCF";
+	std::string trackingAlg = "KCF";
 
-  // set the tracking algorithm from parameter
-  if(argc>2)
-    trackingAlg = argv[2];
+	if(argc>2)
+	trackingAlg = argv[2];
 
-  // create the tracker
-  MultiTracker trackers(trackingAlg);
+	MultiTracker trackers;
 
-  // container of the tracked objects
-  vector<Rect2d> objects;
+	vector<Rect2d> objects;
 
-  // set input video
-  std::string video = argv[1];
-  VideoCapture cap(video);
+	std::string video = argv[1];
+	VideoCapture cap(video);
 
-  Mat frame;
+	Mat frame;
 
-  // get bounding box
-  cap >> frame;
-  selectROI("tracker",frame,objects);
+	// get bounding box
+	cap >> frame;
+	vector<Rect> rois;
+	selectROIs("tracker",frame, rois);
 
-  //quit when the tracked object(s) is not provided
-  if(objects.size()<1)
-    return 0;
+	//quit when the tracked object(s) is not provided
+	if(rois.size()<1)
+		return 0;
 
-  // initialize the tracker
-  trackers.add(frame,objects);
+	std::vector<Ptr<Tracker> > algorithms;
+	for (size_t i = 0; i < rois.size(); i++)
+	{
+		algorithms.push_back(createTrackerByName(trackingAlg));
+		objects.push_back(rois[i]);
+	}
 
-  // do the tracking
-  printf("Start the tracking process, press ESC to quit.\n");
-  for ( ;; ){
-    // get frame from the video
-    cap >> frame;
+	// initialize the tracker
+	trackers.add(algorithms, frame, objects);
 
-    // stop the program if no more images
-    if(frame.rows==0 || frame.cols==0)
-      break;
+	// do the tracking
+	printf("Start the tracking process, press ESC to quit.\n");
+	for (;;) {
+		// get frame from the video
+		cap >> frame;
 
-    //update the tracking result
-    trackers.update(frame);
+		// stop the program if no more images
+		if(frame.rows==0 || frame.cols==0)
+		break;
 
-    // draw the tracked object
-    for(unsigned i=0;i<trackers.objects.size();i++)
-      rectangle( frame, trackers.objects[i], Scalar( 255, 0, 0 ), 2, 1 );
+		//update the tracking result
+		trackers.update(frame);
 
-    // show image with the tracked object
-    imshow("tracker",frame);
+		// draw the tracked object
+		for(unsigned i=0; i<trackers.getObjects().size(); i++)
+			rectangle( frame, trackers.getObjects()[i], Scalar( 255, 0, 0 ), 2, 1 );
 
-    //quit on ESC button
-    if(waitKey(1)==27)break;
-  }
+		// show image with the tracked object
+		imshow("tracker",frame);
+
+		//quit on ESC button
+		if(waitKey(1)==27)break;
+	}
 
 }
